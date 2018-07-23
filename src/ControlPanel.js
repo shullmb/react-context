3cd98c5788df48d69bbdc6107d323031f4c8d622
shullmb <shullmb@gmail.com>
2018-07-23 14:16:40 -0700

    created ControlPanel component

---
+++ src/ControlPanel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { RootContext } from './RootContext';
+import { ColorButton } from './ColorButton';
+
+export const ControlPanel = props => (
+  <div className="container">
+    <ColorButton />
+  </div>
+)
