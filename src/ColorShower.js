84227e85d68388800e175b2a199e5255d19e6fb0
shullmb <shullmb@gmail.com>
2018-07-23 14:19:26 -0700

    created ColorShower component

---
+++ src/ColorShower.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { RootContext } from './RootContext';
+import { ColorPane } from './ColorPane';
+
+export const ColorShower = props => (
+  <div className="container">
+    <ColorPane />
+  </div>
+)
