2c7c3469f842cacc75457967c476cb806e5d097f
shullmb <shullmb@gmail.com>
2018-07-23 15:03:04 -0700

    import App.css into ColorShower and ControlPanel

--- src/ColorShower.js
+++ src/ColorShower.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RootContext } from './RootContext';
 import { ColorPane } from './ColorPane';
+import './App.css';
 
 export const ColorShower = props => (
   <div className="container">
